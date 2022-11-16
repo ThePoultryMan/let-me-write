73df7d49be2ce4d4afa778f3fa405a512539484b
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-11-16 18:38:23 -0500

    feat: initial commit

---
+++ components/TipTapField.vue
@@ -0,0 +1,41 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from "@tiptap/vue-2";
+import StarterKit from "@tiptap/starter-kit"; // eslint-disable-line
+
+export default {
+  components: {
+    EditorContent
+  },
+
+  data() {
+    return {
+      editor: null,
+    }
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      extensions: [
+        StarterKit,
+      ],
+    })
+  },
+
+  beforeDestroy() {
+    this.editor.destroy();
+  },
+}
+</script>
+
+<style>
+/* stylelint-disable */
+.ProseMirror {
+  border: none;
+  outline: none;
+}
+/* styleling-enable */
+</style>
