7763c1336f107cdf4c47a2e6fbdd56fb52f4cd00
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-11-16 18:38:55 -0500

    feat: initial commit

---
+++ components/SiteHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="bg-gray-300">
+    <h1>Let Me Write</h1>
+  </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <Tutorial />
+  <div>
+    <textarea />
+    <hr class="mx-[448px] mt-16" />
+    <div class="mx-72 mt-2">
+      <client-only>
+        <TipTapField />
+      </client-only>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: "LetMeWrite",
+  head: {
+    title: "Let Me Write"
+  }
 }
 </script>
